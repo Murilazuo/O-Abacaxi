f7d2026388092032b09c2d518e15385ffb1ee9d8
iBeBoi <89034697+iBeBoi@users.noreply.github.com>
2021-10-17 14:29:43 -0300

    Revert "Level1"
    This reverts commit 49b5fb93952fa27ce86fb7eb868b22944d776f7e.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,6 @@ public class Player : MonoBehaviour
     [SerializeField] private WallCheck checkUp, checkDown, checkRight, checkLeft;
     [SerializeField] private bool up, down, right, left;
     [SerializeField] private WallCheck[] wallColision;
-    [SerializeField] private GameObject[] wallCollisionOBJ;
 
     bool fall = false;
     //evento
@@ -97,7 +96,6 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(inputUp) && currentDirection != Vector2.up && !wallColision[0].inColision)
         {
             SetMove(Vector2.up);
-
         }
         else if (Input.GetKeyDown(inputDown) && currentDirection != Vector2.down && !wallColision[1].inColision)
         {
@@ -129,7 +127,7 @@ public class Player : MonoBehaviour
             OnChangedState();
         }
 
-        //canMove = false;
+        canMove = false;
         currentDirection = direction;
         SetDirWallCheck();
         speed = direction * baseSpeed;
@@ -156,9 +154,9 @@ public class Player : MonoBehaviour
                 speed = speed.normalized * -1 * baseSpeed;
                 Reverse();
                 break;
-            /*case "Wall":
+            case "Wall":
                 canMove = true;
-                break;*/
+                break;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
